1cf59648e958791e5708baa23715aa398e821fef
Luka <lukagaxokidze28@gmail.com>
2025-02-11 00:45:09 +0400

    routing title update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ export const routes: Routes = [
     path: '',
     loadComponent: () =>
       import('./features/home/home.component').then((c) => c.HomeComponent),
+    title: 'Browse music | Home page',
   },
 
   {
